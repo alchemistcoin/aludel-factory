85073aae617e5e169a08e6284044bd7e0d933293
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2022-02-07 18:54:21 -0300

    fix some errors

--- src/AludelFactory.sol
+++ src/AludelFactory.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.6;
 
-import { ProxyFactory } from './ProxyFactory.sol';
-import { Clones } from '@openzeppelin/contracts/access/Ownable.sol';
+import { ProxyFactory } from './factory/ProxyFactory.sol';
+import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
+import { IAludel } from './aludel/IAludel.sol';
 
 contract AludelFactory is Ownable {
     /// @notice array of template addresses
@@ -16,10 +17,10 @@ contract AludelFactory is Ownable {
     /// @notice perform a minimal proxy deploy
     /// @param templateId the number of the template to launch
     /// @param data the calldata to use on the new aludel initialization
-    /// @return the new aludel address.
+    /// @return aludel the new aludel address.
 	function launch(uint256 templateId, bytes calldata data) public returns (address aludel) {
         // get the aludel template address
-		address aludel = _templates[templateId];
+		address template = _templates[templateId];
 
 		// create clone and initialize
 		aludel = ProxyFactory._create(
@@ -28,13 +29,13 @@ contract AludelFactory is Ownable {
         );
 
 		// emit event
-		emit AludelSpawn(aludel);
+		emit AludelSpawned(aludel);
 
 		// explicit return
 		return aludel;
 	}
 
-	function addTemplate(address template) public view onlyOwner {
+	function addTemplate(address template) public onlyOwner {
 		// do we need any checks here?
         require(template != address(0), "invalid template");
 
@@ -44,7 +45,7 @@ contract AludelFactory is Ownable {
         // bleep?
 	}
 
-	function getTemplate(uint256 templateId) public view {
-		return _templates[id];
+	function getTemplate(uint256 templateId) public view returns (address) {
+		return _templates[templateId];
 	}
 }
