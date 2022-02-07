bf88dcd00c49b0dade0cbeca44b4edf6e36b05ce
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2022-02-07 18:54:50 -0300

    add tests

--- src/AludelFactory.t.sol
+++ src/AludelFactory.t.sol
@@ -4,19 +4,34 @@ pragma solidity ^0.8.6;
 import "ds-test/test.sol";
 
 import "./AludelFactory.sol";
+import "./aludel/Aludel.sol";
+
+contract User {
+    constructor() {}
+
+}
 
 contract AludelFactoryTest is DSTest {
     AludelFactory factory;
-
+    User user;
     function setUp() public {
         factory = new AludelFactory();
+        user = new User();
     }
 
     function testFail_basic_sanity() public {
         assertTrue(false);
     }
 
-    function test_basic_sanity() public {
-        assertTrue(true);
+    function test_full() public {
+        Aludel aludel = new Aludel();
+        factory.addTemplate(address(aludel));
+        // address ownerAddress,
+        // address rewardPoolFactory,
+        // address powerSwitchFactory,
+        // address stakingToken,
+        // address rewardToken,
+        // RewardScaling memory rewardScaling
+        factory.launch(0, )
     }
 }
