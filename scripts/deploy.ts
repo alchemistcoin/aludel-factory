96a24e3f2199202ef3ab46e63b6e9fd45c77e7c2
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2022-02-09 18:27:13 -0300

    deployTx.wait is not working for some reason

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,5 +1,7 @@
 import { ethers, run } from "hardhat";
 
+const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
+
 async function main() {
 
   const signer = (await ethers.getSigners())[0]
@@ -8,29 +10,34 @@ async function main() {
   // We get the contract to deploy
   const AludelFactory = await ethers.getContractFactory("AludelFactory");
   const factory = await AludelFactory.deploy();
+  await factory.deployed()
   console.log("AludelFactory deployed to:", factory.address);
   
-  await factory.deployTransaction.wait(5)
+  // await factory.deployTransaction.wait(1)
+  await sleep(150000)  
   // verify source
   console.log('Verifying source on etherscan')
   await run('verify:verify', {
     address: factory.address,
     constructorArguments: [],
   })
-  
-  
+
   const AludelTemplate = await ethers.getContractFactory("Aludel");
   const template = await AludelTemplate.deploy();
+  await template.deployed()
+  console.log("AludelTemplate deployed to:", template.address);
   
-  await template.deployTransaction.wait(5)
+  // await template.deployTransaction.wait(1)
+  await sleep(150000)  
+  
   // verify source
   console.log('Verifying source on etherscan')
   await run('verify:verify', {
     address: template.address,
     constructorArguments: [],
   })
-  console.log("AludelTemplate deployed to:", template.address);
 
+  // add template
   await factory.addTemplate(template.address)
 
 }
