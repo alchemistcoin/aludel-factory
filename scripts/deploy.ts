3be99244adcfa552e4ef44366ce9543ccf9e517c
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2022-02-09 15:14:10 -0300

    verify contracts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,40 @@
-import { ethers } from "hardhat";
+import { ethers, run } from "hardhat";
 
 async function main() {
+
+  const signer = (await ethers.getSigners())[0]
+  console.log('Signer:', signer.address)
 
   // We get the contract to deploy
   const AludelFactory = await ethers.getContractFactory("AludelFactory");
   const factory = await AludelFactory.deploy();
-  await factory.deployed();
-
-  const AludelTemplate = await ethers.getContractFactory("AludelTemplate");
+  console.log("AludelFactory deployed to:", factory.address);
+  
+  await factory.deployTransaction.wait(5)
+  // verify source
+  console.log('Verifying source on etherscan')
+  await run('verify:verify', {
+    address: factory.address,
+    constructorArguments: [],
+  })
+  
+  
+  const AludelTemplate = await ethers.getContractFactory("Aludel");
   const template = await AludelTemplate.deploy();
-  await template.deployed();
+  
+  await template.deployTransaction.wait(5)
+  // verify source
+  console.log('Verifying source on etherscan')
+  await run('verify:verify', {
+    address: template.address,
+    constructorArguments: [],
+  })
+  console.log("AludelTemplate deployed to:", template.address);
 
   await factory.addTemplate(template.address)
 
-  console.log("AludelFactory deployed to:", factory.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
