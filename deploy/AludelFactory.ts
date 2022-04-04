18c58b92d6e92fbb5f34a53e694aee32e6a91cf5
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2022-04-04 00:47:59 -0300

    lock aludel templates after deploy it.

--- deploy/AludelFactory.ts
+++ deploy/AludelFactory.ts
@@ -1,5 +1,7 @@
-import { DeployResult } from "hardhat-deploy/dist/types";
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-deploy";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Contract, ContractFactory } from '@ethersproject/contracts'
 
 export default async function ({
     ethers,
@@ -12,19 +14,23 @@ export default async function ({
     async function deployTemplate(
         templateName: string,
         libraries?: {[key: string]: string
-    } ): Promise<DeployResult> {
-        return deploy(templateName, {
+    } ): Promise<Contract> {
+    // } ): Promise<[DeployResult, Contract]> {
+            const result = await deploy(templateName, {
             from: deployer,
             args: [],
             log: true,
             libraries,
             deterministicDeployment: false,
         });
+        return ethers.getContractAt(templateName, result.address)
     }
 
     const { deploy } = deployments;
     const { deployer, dev } = await getNamedAccounts();
     
+    console.log(deployer)
+
     // deploy factory 
     const deployedFactory = await deploy("AludelFactory", {
         from: deployer,
@@ -42,19 +48,28 @@ export default async function ({
 
     // deploy aludel v1.5
     const aludel = await deployTemplate('Aludel')
+    await aludel.initializeLock();
+    
+
     // deploy aludel timed lock
     const aludelTimedLock = await deployTemplate('AludelTimedLock', {
         FIFO: fifoLibrary.address
     })
-    
+    await aludelTimedLock.initializeLock();
+
     const factory = await ethers.getContractAt('AludelFactory', deployedFactory.address)
     
+    console.log('Adding templates to', factory.address)
     // add templates in factory
     if (!(await factory.isInstance(aludel.address))) {
         await factory.addTemplate(aludel.address)
+    } else {
+        console.log('Skipping', aludel.address)
     }
     
     if (!(await factory.isInstance(aludelTimedLock.address))) {
         await factory.addTemplate(aludelTimedLock.address)
+    } else {
+        console.log('Skipping', aludelTimedLock.address)
     }
 };
