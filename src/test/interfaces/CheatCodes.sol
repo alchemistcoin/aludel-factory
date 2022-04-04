6f30e123bef9b1415c034c1bfd62dee5a7f23265
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2022-04-04 00:46:54 -0300

    add assume cheatcode

--- src/test/interfaces/CheatCodes.sol
+++ src/test/interfaces/CheatCodes.sol
@@ -55,4 +55,6 @@ interface CheatCodes {
     function getCode(string calldata) external returns (bytes memory);
     // Label an address in test traces
     function label(address addr, string memory label) external;
+    // When fuzzing, generate new inputs if conditional not met
+    function assume(bool) external;
 }
