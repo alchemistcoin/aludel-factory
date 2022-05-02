c78625f36d780a6cba0d7fac81814f3d05fa9fc1
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2022-05-02 11:38:18 -0300

    import alchemist lib files

--- src/contracts/AludelFactory.sol
+++ src/contracts/AludelFactory.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.6;
 
-import { ProxyFactory } from './factory/ProxyFactory.sol';
+import { ProxyFactory } from 'alchemist/factory/ProxyFactory.sol';
 import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
 import { IAludel } from './aludel/IAludel.sol';
-import { InstanceRegistry } from "./factory/InstanceRegistry.sol";
+import { InstanceRegistry } from "alchemist/factory/InstanceRegistry.sol";
 
 contract AludelFactory is Ownable, InstanceRegistry {
     /// @notice array of template addresses
