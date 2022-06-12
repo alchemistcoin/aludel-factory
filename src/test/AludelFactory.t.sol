54f135be4bd57a646f02d4d2e822080f75fa627b
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2022-06-12 12:36:01 -0300

    format

--- src/test/AludelFactory.t.sol
+++ src/test/AludelFactory.t.sol
@@ -1,280 +1,303 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.13;
 
-import 'ds-test/test.sol';
-import 'solmate/tokens/ERC20.sol';
+import "ds-test/test.sol";
+import "solmate/tokens/ERC20.sol";
 import {Hevm} from "solmate/test/utils/Hevm.sol";
 
-import {AludelFactory} from '../contracts/AludelFactory.sol';
-import {Aludel} from '../contracts/aludel/Aludel.sol';
-import { IAludel } from '../contracts/aludel/IAludel.sol';
-import {RewardPoolFactory} from 'alchemist/aludel/RewardPoolFactory.sol';
-import {PowerSwitchFactory} from '../contracts/powerSwitch/PowerSwitchFactory.sol';
+import {AludelFactory} from "../contracts/AludelFactory.sol";
+import {Aludel} from "../contracts/aludel/Aludel.sol";
+import {IAludel} from "../contracts/aludel/IAludel.sol";
+import {RewardPoolFactory} from "alchemist/aludel/RewardPoolFactory.sol";
+import {PowerSwitchFactory} from "../contracts/powerSwitch/PowerSwitchFactory.sol";
 
 import {IFactory} from "alchemist/factory/IFactory.sol";
 
-import {IUniversalVault, Crucible} from 'alchemist/crucible/Crucible.sol';
-import {CrucibleFactory } from 'alchemist/crucible/CrucibleFactory.sol';
+import {IUniversalVault, Crucible} from "alchemist/crucible/Crucible.sol";
+import {CrucibleFactory} from "alchemist/crucible/CrucibleFactory.sol";
 import {ERC721Holder} from "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
-import {MockERC20} from './mocks/MockERC20.sol';
+import {MockERC20} from "./mocks/MockERC20.sol";
 
 import {EnumerableSet} from "../contracts/libraries/EnumerableSet.sol";
 
 contract AludelFactoryTest is DSTest {
-
-	AludelFactory factory;
+    AludelFactory factory;
     Hevm cheats;
-	IAludel aludel;
+    IAludel aludel;
 
-	MockERC20 stakingToken;
-	MockERC20 rewardToken;
-	address owner;
-	address crucible;
+    MockERC20 stakingToken;
+    MockERC20 rewardToken;
+    address owner;
+    address crucible;
 
-	address public constant CRUCIBLE_FACTORY = 0x54e0395CFB4f39beF66DBCd5bD93Cca4E9273D56;
+    address public constant CRUCIBLE_FACTORY =
+        0x54e0395CFB4f39beF66DBCd5bD93Cca4E9273D56;
 
-	uint248 public constant PRIVATE_KEY = type(uint248).max >> 7;
+    uint248 public constant PRIVATE_KEY = type(uint248).max >> 7;
 
-	RewardPoolFactory rewardPoolFactory;
-	PowerSwitchFactory powerSwitchFactory;
-	RewardScaling rewardScaling;
- 
-	struct RewardScaling {
-		uint256 floor;
-		uint256 ceiling;
-		uint256 time;
-	}
+    RewardPoolFactory rewardPoolFactory;
+    PowerSwitchFactory powerSwitchFactory;
+    RewardScaling rewardScaling;
 
-	struct AludelInitializationParams {
-		address rewardPoolFactory;
-		address powerSwitchFactory;
-		address stakingToken;
-		address rewardToken;
-		RewardScaling rewardScaling;
-	}
-
-	function setUp() public {
-		cheats = Hevm(HEVM_ADDRESS);
-		factory = new AludelFactory();
-
-		Aludel template = new Aludel();
-		template.initializeLock();
-		rewardPoolFactory = new RewardPoolFactory();
-		powerSwitchFactory = new PowerSwitchFactory();
-		// Crucible crucibleTemplate = new Crucible();
-		// crucibleTemplate.initializeLock();
-		// CrucibleFactory crucibleFactory = new CrucibleFactory(address(crucibleTemplate));
-		IFactory crucibleFactory = IFactory(address(CRUCIBLE_FACTORY));
-		stakingToken = new MockERC20('', 'STK');
-		rewardToken = new MockERC20('', 'RWD');
-
-		address[] memory bonusTokens = new address[](2);
-		bonusTokens[0] = address(new MockERC20('', 'BonusToken A'));
-		bonusTokens[1] = address(new MockERC20('', 'BonusToken B'));
-
-		rewardScaling = RewardScaling({ floor: 1 ether, ceiling: 10 ether, time: 1 days });
-
-		AludelInitializationParams memory params = AludelInitializationParams({
-			rewardPoolFactory: address(rewardPoolFactory),
-			powerSwitchFactory: address(powerSwitchFactory),
-			stakingToken: address(stakingToken),
-			rewardToken: address(rewardToken),
-			rewardScaling: rewardScaling
-		});
-
-		owner = cheats.addr(PRIVATE_KEY);
-
-		factory.addTemplate(address(template), "test template");
-
-		uint64 startTime = uint64(block.timestamp);
-
-
-		aludel = IAludel(factory.launch(
-			address(template), 
-			"name",
-			"https://staking.token", 
-			startTime,
-			CRUCIBLE_FACTORY,
-			bonusTokens,
-			owner,
-			abi.encode(params))
-		);
-		assertEq(aludel.getBonusTokenSetLength(), 2);
-		AludelFactory.ProgramData memory program = factory.getProgram(address(aludel));
-		
-		assertEq(program.name, "name");
-		assertEq(program.template, address(template));
-		assertEq(program.startTime, block.timestamp);
-
-		IAludel.AludelData memory data = aludel.getAludelData();
-		
-		MockERC20(data.rewardToken).mint(owner, 1 ether);
-		cheats.startPrank(owner);
-		MockERC20(data.rewardToken).approve(address(aludel), 1 ether);
-		aludel.fund(1 ether, 1 days);
-		aludel.registerVaultFactory(address(template));
-		cheats.stopPrank();
-
-		MockERC20(data.stakingToken).mint(owner, 1 ether);
-
-		cheats.prank(owner);
-		crucible = crucibleFactory.create('');
-		MockERC20(data.stakingToken).mint(crucible, 1 ether);
-	}
-
-	function test_ownership() public {
-		assertEq(factory.owner(), address(this));
-	}
-
-	function test_template_initialization() public {
-		Aludel template = new Aludel();
-		template.initializeLock();
-		factory.addTemplate(address(template), "bleep");
-	}
-
-	function test_disable_template() public {
-		Aludel template = new Aludel();
-		template.initializeLock();
-		// expect emit
-		uint256 templateIndex = factory.addTemplate(address(template), "bloop") - 1;
-
-		EnumerableSet.TemplateData[] memory templates = factory.getTemplates();
-		// template should not be disabled
-		assertTrue(templates[templateIndex].disabled == false);
-
-		// disable template
-		factory.updateTemplate(address(template), true);
-
-		templates = factory.getTemplates();
-		// now template is disabled
-		assertTrue(templates[templateIndex].disabled == true);
-
-	}
-
-	function test_launch_with_disable_template() public {
-		Aludel template = new Aludel();
-		template.initializeLock();
-		// expect emit
-		factory.addTemplate(address(template), "foo");
-		// disable template
-		factory.updateTemplate(address(template), true);
-
-		AludelInitializationParams memory params = AludelInitializationParams({
-			rewardPoolFactory: address(rewardPoolFactory),
-			powerSwitchFactory: address(powerSwitchFactory),
-			stakingToken: address(stakingToken),
-			rewardToken: address(rewardToken),
-			rewardScaling: rewardScaling
-		});
-
-		cheats.expectRevert(AludelFactory.TemplateDisabled.selector);
-		uint64 startTime = uint64(block.timestamp);
-		address[] memory bonusTokens = new address[](0);
-		factory.launch(
-			address(template),
-			"name",
-			"https://staking.token",
-			startTime,
-			CRUCIBLE_FACTORY,
-			bonusTokens,
-			owner,
-			abi.encode(params)
-		);
-	}
-
-	function testFail_template_double_initialization() public {
-		Aludel template = new Aludel();
-		template.initializeLock();
-		cheats.expectRevert(new bytes(0));
-		template.initializeLock();
-	}
-
-	function test_stake() public {
-
-		bytes memory permission = getPermission(
-			PRIVATE_KEY,
-			'Lock',
-			crucible,
-			address(aludel),
-			address(stakingToken),
-			1 ether
-		);
-		cheats.prank(owner);
-		aludel.stake(crucible, 1 ether, permission);
-	}
-
-	function test_unstake() public {
-
-		bytes memory lockPermission = getPermission(
-			PRIVATE_KEY,
-			'Lock',
-			crucible,
-			address(aludel),
-			address(stakingToken),
-			1 ether
-		);
-		cheats.prank(owner);
-		aludel.stake(crucible, 1 ether, lockPermission);
-		bytes memory unlockPermission = getPermission(
-			PRIVATE_KEY,
-			'Unlock',
-			crucible,
-			address(aludel),
-			address(stakingToken),
-			1 ether
-		);
-		cheats.prank(owner);
-		aludel.unstakeAndClaim(crucible, 1 ether, unlockPermission);
-
-	}
-
-
-	function getPermission(
-		uint256 privateKey,
-		string memory method,
-		address crucible,
-		address delegate,
-		address token,
-		uint256 amount
-	) public returns(bytes memory) {
-		
-		uint256 nonce = IUniversalVault(crucible).getNonce();
-		
-        bytes32 domainSeparator = keccak256(abi.encode(
-			keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-			keccak256('UniversalVault'),
-			keccak256('1.0.0'),
-			getChainId(),
-			crucible
-		));
-		bytes32 structHash = keccak256(abi.encode(
-			keccak256(abi.encodePacked(method, "(address delegate,address token,uint256 amount,uint256 nonce)")),
-			address(delegate),
-			address(token),
-			amount,
-			nonce
-		));
-
-		bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
-
-		(uint8 v, bytes32 r, bytes32 s) = cheats.sign(privateKey, digest);
-
-		return joinSignature(r, s, v);
-	}
-
-	/// 
-
-    function getChainId() internal view returns (uint chainId) {
-        assembly { chainId := chainid() }
+    struct RewardScaling {
+        uint256 floor;
+        uint256 ceiling;
+        uint256 time;
     }
 
-	function joinSignature(bytes32 r, bytes32 s, uint8 v) internal returns (bytes memory) {
-		bytes memory sig = new bytes(65);
-		assembly {
-			mstore(add(sig, 0x20), r)
-			mstore(add(sig, 0x40), s)
-			mstore8(add(sig, 0x60), v)
-		}
-		return sig;
-	}
+    struct AludelInitializationParams {
+        address rewardPoolFactory;
+        address powerSwitchFactory;
+        address stakingToken;
+        address rewardToken;
+        RewardScaling rewardScaling;
+    }
+
+    function setUp() public {
+        cheats = Hevm(HEVM_ADDRESS);
+        factory = new AludelFactory();
+
+        Aludel template = new Aludel();
+        template.initializeLock();
+        rewardPoolFactory = new RewardPoolFactory();
+        powerSwitchFactory = new PowerSwitchFactory();
+        // Crucible crucibleTemplate = new Crucible();
+        // crucibleTemplate.initializeLock();
+        // CrucibleFactory crucibleFactory = new CrucibleFactory(address(crucibleTemplate));
+        IFactory crucibleFactory = IFactory(address(CRUCIBLE_FACTORY));
+        stakingToken = new MockERC20("", "STK");
+        rewardToken = new MockERC20("", "RWD");
+
+        address[] memory bonusTokens = new address[](2);
+        bonusTokens[0] = address(new MockERC20("", "BonusToken A"));
+        bonusTokens[1] = address(new MockERC20("", "BonusToken B"));
+
+        rewardScaling = RewardScaling({
+            floor: 1 ether,
+            ceiling: 10 ether,
+            time: 1 days
+        });
+
+        AludelInitializationParams memory params = AludelInitializationParams({
+            rewardPoolFactory: address(rewardPoolFactory),
+            powerSwitchFactory: address(powerSwitchFactory),
+            stakingToken: address(stakingToken),
+            rewardToken: address(rewardToken),
+            rewardScaling: rewardScaling
+        });
+
+        owner = cheats.addr(PRIVATE_KEY);
+
+        factory.addTemplate(address(template), "test template");
+
+        uint64 startTime = uint64(block.timestamp);
+
+        aludel = IAludel(
+            factory.launch(
+                address(template),
+                "name",
+                "https://staking.token",
+                startTime,
+                CRUCIBLE_FACTORY,
+                bonusTokens,
+                owner,
+                abi.encode(params)
+            )
+        );
+        assertEq(aludel.getBonusTokenSetLength(), 2);
+        AludelFactory.ProgramData memory program = factory.getProgram(
+            address(aludel)
+        );
+
+        assertEq(program.name, "name");
+        assertEq(program.template, address(template));
+        assertEq(program.startTime, block.timestamp);
+
+        IAludel.AludelData memory data = aludel.getAludelData();
+
+        MockERC20(data.rewardToken).mint(owner, 1 ether);
+        cheats.startPrank(owner);
+        MockERC20(data.rewardToken).approve(address(aludel), 1 ether);
+        aludel.fund(1 ether, 1 days);
+        aludel.registerVaultFactory(address(template));
+        cheats.stopPrank();
+
+        MockERC20(data.stakingToken).mint(owner, 1 ether);
+
+        cheats.prank(owner);
+        crucible = crucibleFactory.create("");
+        MockERC20(data.stakingToken).mint(crucible, 1 ether);
+    }
+
+    function test_ownership() public {
+        assertEq(factory.owner(), address(this));
+    }
+
+    function test_template_initialization() public {
+        Aludel template = new Aludel();
+        template.initializeLock();
+        factory.addTemplate(address(template), "bleep");
+    }
+
+    function test_disable_template() public {
+        Aludel template = new Aludel();
+        template.initializeLock();
+        // expect emit
+        uint256 templateIndex = factory.addTemplate(
+            address(template),
+            "bloop"
+        ) - 1;
+
+        EnumerableSet.TemplateData[] memory templates = factory.getTemplates();
+        // template should not be disabled
+        assertTrue(templates[templateIndex].disabled == false);
+
+        // disable template
+        factory.updateTemplate(address(template), true);
+
+        templates = factory.getTemplates();
+        // now template is disabled
+        assertTrue(templates[templateIndex].disabled == true);
+    }
+
+    function test_launch_with_disable_template() public {
+        Aludel template = new Aludel();
+        template.initializeLock();
+        // expect emit
+        factory.addTemplate(address(template), "foo");
+        // disable template
+        factory.updateTemplate(address(template), true);
+
+        AludelInitializationParams memory params = AludelInitializationParams({
+            rewardPoolFactory: address(rewardPoolFactory),
+            powerSwitchFactory: address(powerSwitchFactory),
+            stakingToken: address(stakingToken),
+            rewardToken: address(rewardToken),
+            rewardScaling: rewardScaling
+        });
+
+        cheats.expectRevert(AludelFactory.TemplateDisabled.selector);
+        uint64 startTime = uint64(block.timestamp);
+        address[] memory bonusTokens = new address[](0);
+        factory.launch(
+            address(template),
+            "name",
+            "https://staking.token",
+            startTime,
+            CRUCIBLE_FACTORY,
+            bonusTokens,
+            owner,
+            abi.encode(params)
+        );
+    }
+
+    function testFail_template_double_initialization() public {
+        Aludel template = new Aludel();
+        template.initializeLock();
+        cheats.expectRevert(new bytes(0));
+        template.initializeLock();
+    }
+
+    function test_stake() public {
+        bytes memory permission = getPermission(
+            PRIVATE_KEY,
+            "Lock",
+            crucible,
+            address(aludel),
+            address(stakingToken),
+            1 ether
+        );
+        cheats.prank(owner);
+        aludel.stake(crucible, 1 ether, permission);
+    }
+
+    function test_unstake() public {
+        bytes memory lockPermission = getPermission(
+            PRIVATE_KEY,
+            "Lock",
+            crucible,
+            address(aludel),
+            address(stakingToken),
+            1 ether
+        );
+        cheats.prank(owner);
+        aludel.stake(crucible, 1 ether, lockPermission);
+        bytes memory unlockPermission = getPermission(
+            PRIVATE_KEY,
+            "Unlock",
+            crucible,
+            address(aludel),
+            address(stakingToken),
+            1 ether
+        );
+        cheats.prank(owner);
+        aludel.unstakeAndClaim(crucible, 1 ether, unlockPermission);
+    }
+
+    function getPermission(
+        uint256 privateKey,
+        string memory method,
+        address crucible,
+        address delegate,
+        address token,
+        uint256 amount
+    ) public returns (bytes memory) {
+        uint256 nonce = IUniversalVault(crucible).getNonce();
+
+        bytes32 domainSeparator = keccak256(
+            abi.encode(
+                keccak256(
+                    "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+                ),
+                keccak256("UniversalVault"),
+                keccak256("1.0.0"),
+                getChainId(),
+                crucible
+            )
+        );
+        bytes32 structHash = keccak256(
+            abi.encode(
+                keccak256(
+                    abi.encodePacked(
+                        method,
+                        "(address delegate,address token,uint256 amount,uint256 nonce)"
+                    )
+                ),
+                address(delegate),
+                address(token),
+                amount,
+                nonce
+            )
+        );
+
+        bytes32 digest = keccak256(
+            abi.encodePacked("\x19\x01", domainSeparator, structHash)
+        );
+
+        (uint8 v, bytes32 r, bytes32 s) = cheats.sign(privateKey, digest);
+
+        return joinSignature(r, s, v);
+    }
+
+    ///
+
+    function getChainId() internal view returns (uint chainId) {
+        assembly {
+            chainId := chainid()
+        }
+    }
+
+    function joinSignature(
+        bytes32 r,
+        bytes32 s,
+        uint8 v
+    ) internal returns (bytes memory) {
+        bytes memory sig = new bytes(65);
+        assembly {
+            mstore(add(sig, 0x20), r)
+            mstore(add(sig, 0x40), s)
+            mstore8(add(sig, 0x60), v)
+        }
+        return sig;
+    }
 }
