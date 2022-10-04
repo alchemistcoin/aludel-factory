2993fc418147d7fa7269e3f9f586697b6d476c4f
paco <plaintextpaco@gmx.com>
2022-10-04 13:09:28 -0300

    cleaned unitary tests
    - [ ] renamed all names to gherkin style
    - [ ] removed unneeded dependencies & setup
    - [ ] unitary tests are not at all reliant on a particular Aludel's
      behaviour 😎

--- src/test/AludelFactory.t.sol
+++ src/test/AludelFactory.t.sol
@@ -8,18 +8,9 @@ import {Hevm} from "solmate/test/utils/Hevm.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 import {AludelFactory} from "../contracts/AludelFactory.sol";
-import {Aludel} from "../contracts/aludel/Aludel.sol";
 import {IAludel} from "../contracts/aludel/IAludel.sol";
-import {RewardPoolFactory} from "alchemist/contracts/aludel/RewardPoolFactory.sol";
-import {PowerSwitchFactory} from "../contracts/powerSwitch/PowerSwitchFactory.sol";
 
-import {IFactory} from "alchemist/contracts/factory/IFactory.sol";
-
-import {IUniversalVault, Crucible} from "alchemist/contracts/crucible/Crucible.sol";
-import {CrucibleFactory} from "alchemist/contracts/crucible/CrucibleFactory.sol";
-import {ERC721Holder} from "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import {MockERC20} from "../contracts/mocks/MockERC20.sol";
-
 import {Spy} from "./Spy.sol";
 
 import {EnumerableSet} from "../contracts/libraries/EnumerableSet.sol";
@@ -31,43 +22,22 @@ contract AludelFactoryTest is DSTest {
     IAludel private aludel;
     Spy private spyTemplate;
 
-    MockERC20 private stakingToken;
-    MockERC20 private rewardToken;
     address private owner;
-    address private crucible;
 
     address[] private bonusTokens;
 
     uint248 public constant PRIVATE_KEY = type(uint248).max >> 7;
 
-    RewardPoolFactory private rewardPoolFactory;
-    PowerSwitchFactory private powerSwitchFactory;
-    RewardScaling private rewardScaling;
-    Aludel private otherAludel;
-    Aludel private template;
-
-    CrucibleFactory private crucibleFactory;
+    IAludel private preexistingAludel;
+    IAludel private listedTemplate;
+    IAludel private unlistedTemplate;
 
     address private recipient;
     uint16 private bps;
-
-    struct RewardScaling {
-        uint256 floor;
-        uint256 ceiling;
-        uint256 time;
-    }
-
-    struct AludelInitializationParams {
-        address rewardPoolFactory;
-        address powerSwitchFactory;
-        address stakingToken;
-        address rewardToken;
-        RewardScaling rewardScaling;
-    }
+    address private constant CRUCIBLE_FACTORY = address(0xcc1b13);
+    uint64 private constant START_TIME = 1234567;
 
     uint256 public constant BASE_SHARES_PER_WEI = 1000000;
-
-    AludelInitializationParams private defaultParams;
 
     function setUp() public {
         cheats = Hevm(HEVM_ADDRESS);
@@ -78,73 +48,32 @@ contract AludelFactoryTest is DSTest {
         bps = 100;
         factory = new AludelFactory(recipient, bps);
 
-        template = new Aludel();
+        listedTemplate = IAludel(address(new Spy()));
         spyTemplate = new Spy();
-        otherAludel = new Aludel();
-        template.initializeLock();
-        rewardPoolFactory = new RewardPoolFactory();
-        powerSwitchFactory = new PowerSwitchFactory();
-
-        Crucible crucibleTemplate = new Crucible();
-        crucibleTemplate.initializeLock();
-        crucibleFactory = new CrucibleFactory(address(crucibleTemplate));
-
-        stakingToken = new MockERC20("", "STK");
-        rewardToken = new MockERC20("", "RWD");
+        preexistingAludel = IAludel(address(new Spy()));
+        unlistedTemplate = IAludel(address(new Spy()));
 
         bonusTokens = new address[](2);
         bonusTokens[0] = address(new MockERC20("", "BonusToken A"));
         bonusTokens[1] = address(new MockERC20("", "BonusToken B"));
 
-        rewardScaling = RewardScaling({
-            floor: 1 ether,
-            ceiling: 10 ether,
-            time: 1 days
-        });
-
-        defaultParams = AludelInitializationParams({
-            rewardPoolFactory: address(rewardPoolFactory),
-            powerSwitchFactory: address(powerSwitchFactory),
-            stakingToken: address(stakingToken),
-            rewardToken: address(rewardToken),
-            rewardScaling: rewardScaling
-        });
-
-        factory.addTemplate(address(template), "test template", false);
+        factory.addTemplate(address(listedTemplate), "test template", false);
         factory.addTemplate(address(spyTemplate), "spy template", false);
-
-        uint64 startTime = uint64(block.timestamp);
 
         aludel = IAludel(
             factory.launch(
-                address(template),
+                address(listedTemplate),
                 "name",
                 "https://staking.token",
-                startTime,
-                address(crucibleFactory),
+                START_TIME,
+                CRUCIBLE_FACTORY,
                 bonusTokens,
                 owner,
-                abi.encode(defaultParams)
+                abi.encode(bytes(""))
             )
         );
-
-        IAludel.AludelData memory data = aludel.getAludelData();
-
-        MockERC20(data.rewardToken).mint(owner, 1 ether);
-
-        cheats.startPrank(owner);
-        MockERC20(data.rewardToken).approve(address(aludel), 1 ether);
-        aludel.fund(1 ether, 1 days);
-        aludel.registerVaultFactory(address(template));
-        cheats.stopPrank();
-
-        data = aludel.getAludelData();
-        MockERC20(data.stakingToken).mint(owner, 1 ether);
-
-        cheats.prank(owner);
-        crucible = crucibleFactory.create("");
-        MockERC20(data.stakingToken).mint(crucible, 1 ether);
     }
+
     function test_WHEN_updating_a_program_with_empty_fields_THEN_it_isnt_updated() public {
         factory.updateProgram(address(aludel), "", "otherurl");
         assertEq(factory.programs(address(aludel)).name, "name");
@@ -164,7 +93,7 @@ contract AludelFactoryTest is DSTest {
                 address(420),
                 bonusTokens,
                 address(69),
-                abi.encode(defaultParams)
+                abi.encode(bytes(""))
             )
         );
         assertTrue(spiedAludel.spyWasCalled(
@@ -188,7 +117,7 @@ contract AludelFactoryTest is DSTest {
                 "coolname",
                 "https://staking.url",
                 420,
-                address(crucibleFactory),
+                CRUCIBLE_FACTORY,
                 bonusTokens,
                 address(69),
                 abi.encode("some data, idk")
@@ -212,16 +141,16 @@ contract AludelFactoryTest is DSTest {
         factory.updateProgram(address(aludel), "othername", "http://stake.other");
         cheats.expectRevert(bytes("Ownable: caller is not the owner"));
         factory.addProgram(
-            address(otherAludel),
-            address(otherAludel),
+            address(preexistingAludel),
+            address(preexistingAludel),
             "name",
             "http://stake.me",
             123
         );
         cheats.expectRevert(bytes("Ownable: caller is not the owner"));
-        factory.addTemplate(address(otherAludel), "test template", false);
+        factory.addTemplate(address(preexistingAludel), "test template", false);
         cheats.expectRevert(bytes("Ownable: caller is not the owner"));
-        factory.updateTemplate(address(template), true);
+        factory.updateTemplate(address(listedTemplate), true);
         cheats.expectRevert(bytes("Ownable: caller is not the owner"));
         factory.setFeeBps(69);
         cheats.expectRevert(bytes("Ownable: caller is not the owner"));
@@ -231,7 +160,7 @@ contract AludelFactoryTest is DSTest {
 
     function test_WHEN_delisting_a_non_listed_program_THEN_it_reverts() public{
         cheats.expectRevert(AludelFactory.AludelNotRegistered.selector);
-        factory.delistProgram(address(otherAludel));
+        factory.delistProgram(address(preexistingAludel));
     }
     function test_WHEN_delisting_a_listed_program_THEN_data_for_it_isnt_available() public{
         factory.delistProgram(address(aludel));
@@ -250,7 +179,7 @@ contract AludelFactoryTest is DSTest {
         factory.delistProgram(address(aludel));
         factory.addProgram(
             address(aludel),
-            address(template),
+            address(listedTemplate),
             "namerino",
             "http://stake.me",
             123
@@ -265,7 +194,7 @@ contract AludelFactoryTest is DSTest {
     function test_WHEN_adding_a_program_manually_AND_using_template_zero_THEN_it_reverts() public {
         cheats.expectRevert(AludelFactory.InvalidTemplate.selector);
         factory.addProgram(
-            address(otherAludel),
+            address(preexistingAludel),
             address(0),
             "name",
             "http://stake.me",
@@ -275,29 +204,29 @@ contract AludelFactoryTest is DSTest {
 
     function test_WHEN_adding_a_program_manually_THEN_the_instance_is_registered_AND_a_program_AND_metadata_can_be_set() public {
         factory.addProgram(
-            address(otherAludel),
-            address(otherAludel),
+            address(preexistingAludel),
+            address(preexistingAludel),
             "name",
             "http://stake.me",
             123
         );
-        assertTrue(factory.isAludel(address(otherAludel)));
-        assertEq(factory.programs(address(otherAludel)).name, "name");
-        factory.updateProgram(address(otherAludel), "othername", "http://stake.other");
-        assertEq(factory.programs(address(otherAludel)).name, "othername");
-        assertEq(factory.programs(address(otherAludel)).stakingTokenUrl, "http://stake.other");
+        assertTrue(factory.isAludel(address(preexistingAludel)));
+        assertEq(factory.programs(address(preexistingAludel)).name, "name");
+        factory.updateProgram(address(preexistingAludel), "othername", "http://stake.other");
+        assertEq(factory.programs(address(preexistingAludel)).name, "othername");
+        assertEq(factory.programs(address(preexistingAludel)).stakingTokenUrl, "http://stake.other");
     }
 
     function test_GIVEN_a_program_wasnt_added_THEN_metadata_for_it_CANNOT_be_set() public {
         cheats.expectRevert(AludelFactory.AludelNotRegistered.selector);
-        factory.updateProgram(address(otherAludel), "othername", "http://stake.other");
+        factory.updateProgram(address(preexistingAludel), "othername", "http://stake.other");
     }
 
     // TODO perhaps we actually want this?
     function test_WHEN_adding_a_program_manually_THEN_it_CANNOT_be_used_as_a_template() public {
         factory.addProgram(
-            address(otherAludel),
-            address(otherAludel),
+            address(preexistingAludel),
+            address(preexistingAludel),
             "name",
             "http://stake.me",
             123
@@ -305,19 +234,19 @@ contract AludelFactoryTest is DSTest {
         cheats.expectRevert(AludelFactory.TemplateNotRegistered.selector);
         aludel = IAludel(
             factory.launch(
-                address(otherAludel),
+                address(preexistingAludel),
                 "name",
                 "https://staking.token",
                 uint64(block.timestamp),
-                address(crucibleFactory),
+                CRUCIBLE_FACTORY,
                 bonusTokens,
                 owner,
-                abi.encode(defaultParams)
+                abi.encode(bytes(""))
             )
         );
     }
 
-    function test_ownership() public {
+    function test_WHEN_deploying_a_factory_THEN_the_owner_is_set_to_the_deployer() public {
         assertEq(factory.owner(), address(this));
     }
 
@@ -326,33 +255,23 @@ contract AludelFactoryTest is DSTest {
         factory.addTemplate(address(0), "idk", true);
     }
 
-    function test_templateNotRegistered() public {
-        Aludel template = new Aludel();
-        template.initializeLock();
-
-        uint32 startTime = 123;
-
+    function test_WHEN_launching_an_aludel_with_an_unlisted_template_THEN_it_reverts_with_TemplateNotRegistered() public {
         cheats.expectRevert(AludelFactory.TemplateNotRegistered.selector);
         factory.launch(
-            address(template),
+            address(unlistedTemplate),
             "name",
             "https://staking.token",
-            startTime,
-            address(crucibleFactory),
+            START_TIME,
+            CRUCIBLE_FACTORY,
             bonusTokens,
             owner,
-            abi.encode(defaultParams)
+            abi.encode(bytes(""))
         );
-
-        factory.addTemplate(address(template), "bleep", false);
     }
 
-    function test_disable_template() public {
-        Aludel template = new Aludel();
-        template.initializeLock();
-        // expect emit
+    function test_WHEN_disabling_a_template_THEN_its_listed_as_disabled() public {
         uint256 templateIndex = factory.addTemplate(
-            address(template),
+            address(unlistedTemplate),
             "bloop",
             false
         ) - 1;
@@ -362,32 +281,28 @@ contract AludelFactoryTest is DSTest {
         assertTrue(templates[templateIndex].disabled == false);
 
         // disable template
-        factory.updateTemplate(address(template), true);
+        factory.updateTemplate(address(unlistedTemplate), true);
 
         templates = factory.getTemplates();
         // now template is disabled
         assertTrue(templates[templateIndex].disabled == true);
     }
 
-    function test_launch_with_disable_template() public {
-        Aludel template = new Aludel();
-        template.initializeLock();
-        // expect emit
-        factory.addTemplate(address(template), "foo", false);
+    function test_WHEN_launching_with_a_disabled_template_THEN_it_reverts() public {
+        factory.addTemplate(address(unlistedTemplate), "foo", false);
         // disable template
-        factory.updateTemplate(address(template), true);
+        factory.updateTemplate(address(unlistedTemplate), true);
 
-        uint64 startTime = uint64(block.timestamp);
         cheats.expectRevert(AludelFactory.TemplateDisabled.selector);
         factory.launch(
-            address(template),
+            address(unlistedTemplate),
             "name",
             "https://staking.token",
-            startTime,
-            address(crucibleFactory),
+            START_TIME,
+            CRUCIBLE_FACTORY,
             bonusTokens,
             owner,
-            abi.encode(defaultParams)
+            abi.encode(bytes(""))
         );
     }
 }
