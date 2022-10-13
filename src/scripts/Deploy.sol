7567561c99d4b39c513dbe95d7bcf2a3e500bbdc
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2022-10-13 10:06:03 -0300

    Deploy old templates and add them to the factory

--- src/scripts/Deploy.sol
+++ src/scripts/Deploy.sol
@@ -10,14 +10,13 @@ import {AludelFactory} from "../contracts/AludelFactory.sol";
 
 import {IAludel} from "../contracts/aludel/IAludel.sol";
 import {Aludel} from "../contracts/aludel/Aludel.sol";
+import {AludelV1} from "../contracts/aludel/legacy/AludelV1.sol";
+import {GeyserV2} from "../contracts/aludel/legacy/GeyserV2.sol";
 
 contract DeployFactory is Script {
 
     function run() external {
         
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-
-
         // default value: 
         address recipient = vm.envAddress("ALUDEL_FACTORY_RECIPIENT");
 
@@ -25,10 +24,43 @@ contract DeployFactory is Script {
         uint16 bps = uint16(vm.envUint("ALUDEL_FACTORY_BPS"));
 
         vm.startBroadcast();
-
         AludelFactory factory = new AludelFactory(recipient, bps);
-        
         vm.stopBroadcast();
+
+        // AludelV1 deployment
+        vm.startBroadcast();
+        AludelV1 aludelV1 = new AludelV1();
+        vm.stopBroadcast();
+
+
+        // add AludelV1 template
+        vm.startBroadcast();
+        factory.addTemplate(address(aludelV1), "AludelV1", true);
+        vm.stopBroadcast();
+
+
+        // AludelV2 deployment
+        vm.startBroadcast();
+        GeyserV2 geyser = new GeyserV2();
+        vm.stopBroadcast();
+
+        // add GeyserV2 template
+        vm.startBroadcast();
+        factory.addTemplate(address(geyser), "GeyserV2", true);
+        vm.stopBroadcast();
+
+
+        // AludelV2 deployment
+        vm.startBroadcast();
+        Aludel aludel = new Aludel();
+        vm.stopBroadcast();
+
+
+        // add AludelV2 template
+        vm.startBroadcast();
+        factory.addTemplate(address(aludel), "AludelV2", false);
+        vm.stopBroadcast();
+
     }
 
 }
