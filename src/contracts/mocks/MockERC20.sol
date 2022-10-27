655665c93f57c4c265ac4e2a89f3c193443caedd
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2022-10-27 16:03:09 -0300

    use OZ ERC20 implementation for MockERC20

--- src/contracts/mocks/MockERC20.sol
+++ src/contracts/mocks/MockERC20.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.6;
 
-import {ERC20} from "@rari-capital/solmate/src/tokens/ERC20.sol";
-import {CrucibleFactory} from "alchemist/contracts/crucible/CrucibleFactory.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
     constructor(string memory name, string memory symbol)
-        ERC20(name, symbol, 18)
+        ERC20(name, symbol)
     {}
 
     function mint(address to, uint256 amount) public {
