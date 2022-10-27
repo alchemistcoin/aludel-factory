9aa237baa22f306413a68865925f3c0c28b6e337
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2022-10-27 16:02:37 -0300

    import Aludel from alchemist repo

--- src/contracts/mocks/imports.sol
+++ src/contracts/mocks/imports.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.8.0;
 
 import { RewardPoolFactory } from "alchemist/contracts/aludel/RewardPoolFactory.sol";
 import { RewardPool } from "alchemist/contracts/aludel/RewardPool.sol";
-import {CrucibleFactory} from "alchemist/contracts/crucible/CrucibleFactory.sol";
+import { CrucibleFactory } from "alchemist/contracts/crucible/CrucibleFactory.sol";
+import { PowerSwitchFactory } from "alchemist/contracts/aludel/PowerSwitchFactory.sol";
+import { Aludel } from "alchemist/contracts/aludel/Aludel.sol";
