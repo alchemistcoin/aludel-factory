0824cfaa43b427470f5c836b157f5519f8217b22
paco <plaintextpaco@gmx.com>
2022-10-31 18:06:57 -0300

    mocha things for eslint
    I want to make sure we don't commit & push tests with .only on them

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,11 @@ module.exports = {
   env: { node: true },
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "prettier"],
+  plugins: ["@typescript-eslint", "prettier", "mocha"],
   rules: {
     "prettier/prettier": "error",
+    "mocha/no-skipped-tests": "error",
+    "mocha/no-exclusive-tests": "error",
   },
   root: true,
 };
